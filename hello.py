c43dd3421c1e0782b7f1f43d67c04ff08e1607da
PilarCha <pchavez92@gmail.com>
2021-08-16 15:23:12 -0700

    setting up routes and methods with a new temploate

--- hello.py
+++ hello.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import sqlite3
+import os
+# this will store the directory of this specific file in a variable
+currentdirectory = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello,World</p>"
+def main():
+    return render_template("main.html")
+
+@app.route("/", method = ["POST"])
+def phonebook():
+    name = request.form["Name"]
+    phonenumber = request.form["Phonenumber"]
+    connection = sqlite3.connect(currentdirectory + "\phonebook.db")
+    cursor = connection.cursor()
+
+if __name__ == "__main__":
+    app.run()
